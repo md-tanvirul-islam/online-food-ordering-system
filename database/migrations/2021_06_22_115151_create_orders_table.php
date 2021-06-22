f38d1f3d16cb2a1842dca4ef98c90b26fca1ee88
Your Name <md.tanvirulislam@outlook.com>
2021-06-22 18:24:05 +0600

    order

---
+++ database/migrations/2021_06_22_115151_create_orders_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->json('address');
+            $table->string('subtotal');
+            $table->string('coupon_code');
+            $table->string('discount_in_amount')->nullable();
+            $table->string('total')->nullable();
+            $table->boolean('is_active')->default(1);
+            $table->unsignedBigInteger('created_by')->nullable();
+            $table->unsignedBigInteger('updated_by')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
