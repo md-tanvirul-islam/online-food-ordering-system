089d5feb0e34cc78d6b19c6d7e3fd6f2f9083e6c
Your Name <md.tanvirulislam@outlook.com>
2021-06-22 18:14:13 +0600

    food

---
+++ database/migrations/2021_06_22_115122_create_food_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFoodTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('food', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('category_id');
+            $table->text('description')->nullable();
+            $table->string('price');
+            $table->string('discount')->nullable();
+            $table->boolean('is_active')->default(1);
+            $table->unsignedBigInteger('created_by')->nullable();
+            $table->unsignedBigInteger('updated_by')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('food');
+    }
+}
